f3a20917718e9cd494e55f4ac6aecda2d2ea82ed
cryptofans <96115271+dannier666@users.noreply.github.com>
2022-08-17 10:38:39 +0800

    Update Staker.sol

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -3,16 +3,7 @@ pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
-
-contract Staker {
-
-  ExampleExternalContract public exampleExternalContract;
-
-  mapping(address => uint256) public balances;
-  mapping(address => uint256) public depositTimestamps;
-
-  uint256 public constant rewardRatePerSecond = 0.1 ether;
-  uint256 public withdrawalDeadline = block.timestamp + 120 seconds;
+.timestamp + 120 seconds;
   uint256 public claimDeadline = block.timestamp + 240 seconds;
   uint256 public currentBlock = 0;
 
